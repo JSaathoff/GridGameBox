dc48c683848aadc061e469ba9aea544b1eef81f4
Johannes <johannes.saathoff@protonmail.com>
2025-12-28 17:09:12 +0100

    test mine placement avoids neighbors of clicked cell

--- src/test/java/dev/saathoff/service/minesweeper/MineServiceTest.java
+++ src/test/java/dev/saathoff/service/minesweeper/MineServiceTest.java
@@ -12,16 +12,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class MineServiceTest {
     @Test
     void placeMines_ShouldPlaceMinesInPredictableLocations_WithFixedSeed() {
-        //GIVEN
+        // GIVEN
         MSGridService gridService = new MSGridService();
         MineService mineService = new MineService();
         Grid<MSCell> grid = gridService.generateNewGrid(5,5);
         Random fixedRandom = new Random(1337);
-        //WHEN
+        // WHEN
         mineService.placeMines(grid, 3, 2,2, fixedRandom);
-        //THEN
+        // THEN
         assertTrue(grid.getCell(2, 0).isMine());
         assertTrue(grid.getCell(1, 4).isMine());
         assertTrue(grid.getCell(1, 0).isMine());
+    }
+
+    @Test
+    void placeMines_ShouldLeaveOutNeighborsOfClickedCell(){
+        // GIVEN
+        MSGridService gridService = new MSGridService();
+        MineService mineService = new MineService();
+        Grid<MSCell> grid = gridService.generateNewGrid(3,3);
+        // WHEN
+        mineService.placeMines(grid, 3, 2,1, new Random());
+        // THEN
+        assertTrue(grid.getCell(0, 0).isMine());
+        assertTrue(grid.getCell(0, 1).isMine());
+        assertTrue(grid.getCell(0, 2).isMine());
     }
 }
